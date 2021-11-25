bafb19e20499216fa0f537132638a7fe02c6dfc6
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-24 20:54:26 -0800

    Changes matched cards values to true

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -36,14 +36,23 @@ const handleChoice = (card) => {
 useEffect(() => {
   if (choiceOne && choiceTwo){
     if (choiceOne.src === choiceTwo.src) {
-      console.log('Cards match')
+     setCards(prevCards => {
+       return prevCards.map((card)=> {
+        if (card.src === choiceOne.src) {
+          return {...card, matched: true }
+        } else {
+          return card
+        }
+       })
+      })
       resetTurn()
     } else {
-      console.log("Cards do not match")
       resetTurn()
     }
   }
- }, [choiceOne, choiceTwo])
+}, [choiceOne,choiceTwo])
+
+console.log(cards)
 
 
 const resetTurn = () => {
